bc03961e2c6e9bc4d484f25cea23a80a5f1ee2fe
Rozey17 <ferrad.fm@gmail.com>
2021-12-14 14:59:55 +0100

    chore: wip

--- libs/shared-ui-components/src/home/hero.tsx
+++ libs/shared-ui-components/src/home/hero.tsx
@@ -1,7 +1,10 @@
 import { useRouter } from 'next/router';
+import { useIntl } from 'react-intl';
 
 export function Hero() {
   const router = useRouter();
+  const { formatMessage: f } = useIntl();
+
   return (
     <section id="hero" className="relative">
       <div className="bg-header-mobile bg-custom-mobile-header-size absolute w-full h-full bg-no-repeat lg:hidden"></div>
@@ -25,7 +28,7 @@ export function Hero() {
               className="hidden lg:block bg-indigo-500 px-7 py-3 rounded-full text-neutral-white text-xs  hover:button-brightness focus:outline-none focus:ring ring-blue-400"
               // className="bg-primary-lime-green px-7 py-3 rounded-full text-neutral-white text-xs bg-gradient-to-r from-primary-lime-green to-primary-bright-cyan hover:button-brightness mb-7 focus:outline-none focus:ring ring-green-400"
             >
-              Créer un compte
+              {f({ id: 'CREATE_ACCOUNT' })}
             </button>
           </div>
         </div>
--- libs/utils/src/locales/messages.ts
+++ libs/utils/src/locales/messages.ts
@@ -1,9 +1,29 @@
 export const messages = {
   en: {
-    login: 'login',
+    LOGIN: 'Login',
+    SIGN_UP: 'Sign up',
+    CREATE_ACCOUNT: 'Create account',
+    HOME: 'Home',
+    ABOUT_US: 'About us',
+    CONTACT_US: 'Contact us',
+    LOGOUT: 'Logout',
+    CAREERS: 'Careers',
+    PRIVACY_POLICY: 'Privacy policy',
+    USERNAME: 'Username',
+    PASSWORD: 'Password',
   },
 
   fr: {
-    login: 'Se Connecter',
+    LOGIN: 'Se Connecter',
+    SIGN_UP: "S'enregistrer",
+    CREATE_ACCOUNT: 'Créer un compte',
+    HOME: 'Accueil',
+    ABOUT_US: 'A propos de nous',
+    CONTACT_US: 'Contactez-nous',
+    LOGOUT: 'Se déconnecter',
+    CAREERS: 'Carrières',
+    PRIVACY_POLICY: 'Politique de confidentialité',
+    USERNAME: "Nom d'utilisateur",
+    PASSWORD: 'Mot de passe',
   },
 };
